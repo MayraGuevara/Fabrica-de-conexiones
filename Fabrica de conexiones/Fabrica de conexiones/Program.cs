186186143f13cb1f0915a44b7a05537ac03c34e4
Mayra Guevara <maystef1@hotmail.com>
2019-05-28 11:31:40 -0600

    iniciando repositorio de fabrica de conexiones

---
+++ Fabrica de conexiones/Fabrica de conexiones/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrica_de_conexiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
